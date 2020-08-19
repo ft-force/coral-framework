5cb6dd8a3ff6ddd41875f3507bbfd581b436655e
JediZ90 <imbh.zhang@outlook.com>
2020-08-19 23:30:24 +0800

    update

--- coral-core/src/main/java/com/ftf/coral/runtime/spring/context/PropertiesUtils.java
+++ coral-core/src/main/java/com/ftf/coral/runtime/spring/context/PropertiesUtils.java
@@ -1,11 +1,15 @@
 package com.ftf.coral.runtime.spring.context;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.EmbeddedValueResolverAware;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringValueResolver;
 
 @Component
 public class PropertiesUtils implements EmbeddedValueResolverAware {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(PropertiesUtils.class);
 
     private static StringValueResolver valueResolver;
 
@@ -14,7 +18,15 @@ public class PropertiesUtils implements EmbeddedValueResolverAware {
         valueResolver = stringValueResolver;
     }
 
-    public static String getPropertiesValue(String name) {
-        return valueResolver.resolveStringValue(name);
+    public static String getPropertiesValue(String key) {
+
+        StringBuilder name = new StringBuilder("${").append(key).append("}");
+
+        try {
+            return valueResolver.resolveStringValue(name.toString());
+        } catch (Throwable e) {
+            LOGGER.warn(e.getMessage());
+            return null;
+        }
     }
 }
